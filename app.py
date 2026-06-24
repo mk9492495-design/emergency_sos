d9408276421ca06984c0cbaca8bb5804d731e3b1
Manpreet Kaur <mk9492495@gmail.com>
2026-06-24 16:58:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, redirect
 
+# Blueprints import
 from login import login_bp
 from register import register_bp
 from dashboard import dashboard_bp
@@ -11,6 +13,12 @@ from sos import sos_bp
 app = Flask(__name__)
 app.secret_key = "emergency_sos_key"
 
+# Default root route - Website khulte hi login page par bhej dega
+@app.route("/")
+def home():
+    return redirect('/login')
+
+# Blueprints Register karna
 app.register_blueprint(login_bp)
 app.register_blueprint(register_bp)
 app.register_blueprint(dashboard_bp)
@@ -20,4 +28,6 @@ app.register_blueprint(history_bp)
 app.register_blueprint(sos_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Render ke liye dynamic port configuration (Render port 10000 deta hai)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
