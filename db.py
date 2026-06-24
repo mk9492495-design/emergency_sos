07e696724ba7d2f816b1e61f450aa1108f3c9a45
Manpreet Kaur <mk9492495@gmail.com>
2026-06-24 16:55:13 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,10 +1,11 @@
+import os
 import pymysql
 
 def get_connection():
     return pymysql.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="emergency_sos",
+        host=os.environ.get("DB_HOST", "localhost"),
+        user=os.environ.get("DB_USER", "root"),
+        password=os.environ.get("DB_PASSWORD", ""),
+        database=os.environ.get("DB_NAME", "emergency_sos"),
         cursorclass=pymysql.cursors.DictCursor
     )
